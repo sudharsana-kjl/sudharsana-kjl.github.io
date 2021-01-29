8e6e2f81261c24ddcf298d52ff86f3f27ed8d4eb
sudharsana-kjl <kjlsudharsana@gmail.com>
2021-01-29 11:07:47 +0530

    new theme src files

---
+++ src/components/Template/SideBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ContactIcons from '../Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const SideBar = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Sudharsana K J L</h2>
+        <p><a href="mailto:kjlsudharsana@gmail.com">kjlsudharsana@gmail.com</a></p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>Hi, I&apos;m Sudharsana. I like building things.
+        I am a <a href="https://www.nitt.edu/">NIT Trichy</a> undergraduate in Computer Science and Engineering, and
+        a Senior Technology Associate at <a href="https://www.morganstanley.com/">Morgan Stanley</a>.
+      </p>
+      <ul className="actions">
+        <li>
+          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ContactIcons />
+    </section>
+  </section>
+);
+
+export default SideBar;
